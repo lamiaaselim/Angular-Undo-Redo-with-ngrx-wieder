fd471c18e7fb5ca53a9b56eb55234677b24b064a
Lamiaa Selim <lamiaaselim1896@gmail.com>
2024-09-10 15:19:25 +0300

    refactor select logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {
   changeMood,
   removeTodo,
   selectList,
-  toggleTodo
+  toggleTodo,
 } from './store/todo/todo.actions';
 import * as fromTodo from './store/todo/todo.selector';
 import * as fromUser from './store/user/user.selector';
@@ -18,7 +18,7 @@ import { updateUser } from './store/user/user.action';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   readonly lists$;
@@ -31,36 +31,38 @@ export class AppComponent {
 
   constructor(private readonly store: Store<State>) {
     this.lists$ = this.store
-      .select(state => state.todo.lists)
-      .pipe(map(lists => Object.values(lists)));
+      .select((state) => state.todo.lists)
+      .pipe(map((lists) => Object.values(lists)));
 
     this.activeList$ = this.store.select(fromTodo.selectActiveList);
 
     this.mood$ = this.store
       .select(fromTodo.selectActiveList)
-      .pipe(select(list => list.mood));
+      .pipe(select((list) => list.mood));
 
     this.todos$ = this.store
       .select(fromTodo.selectActiveList)
-      .pipe(select(list => list.todos));
+      .pipe(select((list) => list.todos));
 
-      this.disableUndo$ = this.store
+    this.disableUndo$ = this.store
       .select(fromTodo.selectCanUndo(undefined)) // Specify the history key here
-      .pipe(map((canUndo) => {
-        console.log('Can Undo:', canUndo);
-        return !canUndo;
-      }));
+      .pipe(
+        map((canUndo) => {
+          console.log('Can Undo:', canUndo);
+          return !canUndo;
+        })
+      );
 
-    this.disableRedo$ = this.store
-      .select(fromTodo.selectCanRedo())
-      .pipe(map((canRedo) => {
+    this.disableRedo$ = this.store.select(fromTodo.selectCanRedo()).pipe(
+      map((canRedo) => {
         console.log('Can Redo:', canRedo);
         return !canRedo;
-      }));
+      })
+    );
 
     this.username$ = this.store
       .select(fromUser.selectUsername)
-      .pipe(map(username => username));
+      .pipe(map((username) => username));
   }
 
   add(text: string): void {
@@ -91,8 +93,14 @@ export class AppComponent {
     this.store.dispatch({ type: 'REDO' });
   }
 
-  selectList(id: string): void {
-    this.store.dispatch(selectList({ id }));
+  // selectList(id: string): void {
+  //   this.store.dispatch(selectList({ id }));
+  // }
+
+  selectList(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    const selectedValue = target.value;
+    this.store.dispatch(selectList({ id: selectedValue }));
   }
 
   updateUsername(username: string) {
